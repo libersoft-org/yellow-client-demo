f945d62f4872da58fc49bbb55a76b0dd4911d284
bizavladimir <fladirmacht@gmail.com>
2023-08-26 15:18:45 +0200

    ..

--- src/components/SocialBar.svelte
+++ src/components/SocialBar.svelte
@@ -6,16 +6,16 @@
 	<img src="img/icons/icon_nemp_v4.svg" alt="Conversations" class="icon" />
 
 	<!-- Contacts Icon -->
-	<img src="img/icons/icon_contacts.svg" alt="Contacts" class="icon" />
+	<!-- <img src="img/icons/icon_contacts.svg" alt="Contacts" class="icon" /> -->
 
 	<!-- Calls Icon -->
-	<img src="img/icons/icon_calls.svg" alt="Calls" class="icon" />
+	<!-- <img src="img/icons/icon_calls.svg" alt="Calls" class="icon" /> -->
 
 	<!-- Newsfeed Icon -->
 	<img src="img/icons/icon_newsfeed.svg" alt="Newsfeed" class="icon" />
 
 	<!-- Videocalls Icon -->
-	<img src="img/icons/icon_videocall.svg" alt="Video calls" class="icon" />
+	<img src="img/icons/icn_play.svg" alt="Video calls" class="icon" />
 
 	<!-- Videomeetings Icon -->
 	<img src="img/icons/icon_video_meeting.svg" alt="Video meetings" class="icon" />
