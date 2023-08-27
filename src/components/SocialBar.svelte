8d855b518097eb533dfb19aa81b8d52246c05718
bizavladimir <fladirmacht@gmail.com>
2023-08-27 05:16:22 +0200

    ..

--- src/components/SocialBar.svelte
+++ src/components/SocialBar.svelte
@@ -1,251 +1,39 @@
 <script>
-    import {onMount} from "svelte";
+    import { dndzone } from 'svelte-dnd-action';
 
-    let draggedItem = null;
-
-    function handleDragStart(event) {
-        draggedItem = event.target.closest('.social-icon-container');
-        event.dataTransfer.effectAllowed = 'move';
-        event.dataTransfer.setData('text/plain', '');
-        event.preventDefault();
-    }
-
-    function handleDragEnter(event) {
-        const target = event.target.closest('.social-icon-container');
-        if (target && draggedItem) {
-            const rect = target.getBoundingClientRect();
-            const halfWidth = rect.width / 2;
-            if (event.clientX - rect.left < halfWidth) {
-                target.parentNode.insertBefore(draggedItem, target);
-            } else {
-                if (target.nextSibling) {
-                    target.parentNode.insertBefore(draggedItem, target.nextSibling);
-                } else {
-                    target.parentNode.appendChild(draggedItem);
-                }
-            }
-        }
-        event.preventDefault()
-    }
-
-    function handleDrop(event) {
-        event.preventDefault();
-        updateIndexes();
-    }
-
-    function handleDragEnd(event) {
-        draggedItem = null;
-        event.preventDefault()
-    }
-
-    function handleDragOver(event) {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'move';
-    }
-
-
-    function updateIndexes() {
-        const icons = document.querySelector('.social-icons-block');
-        const children = Array.from(icons.children);
-        children.forEach((child, index) => {
-            child.setAttribute('data-index', index);
-        });
-    }
+    let items = [
+        { id: 0, title: "Conversations", icon: "icon_nemp_v4.svg", hasDot: true },
+        { id: 1, title: "Forum", icon: "icon_help.svg", hasDot: false },
+        { id: 2, title: "Events", icon: "icon_event.svg", hasDot: false },
+        { id: 3, title: "Blogs", icon: "icon_help.svg", hasDot: false },
+        { id: 4, title: "File sharing", icon: "icon_file_sharing.svg", hasDot: false },
+        { id: 5, title: "E-shops", icon: "icon_help.svg", hasDot: false },
+        { id: 6, title: "Marketplace (with auctions)", icon: "icon_help.svg", hasDot: false },
+        { id: 7, title: "Maps", icon: "icon_map_pin.svg", hasDot: false },
+        { id: 8, title: "Calendar", icon: "icon_calendar.svg", hasDot: false },
+        { id: 9, title: "Tasks", icon: "icon_task.svg", hasDot: false },
+        { id: 10, title: "Donations", icon: "icon_donations.svg", hasDot: false },
+        { id: 11, title: "Surveys", icon: "icon_help.svg", hasDot: false },
+        { id: 12, title: "Payments", icon: "icon_help.svg", hasDot: false },
+        { id: 13, title: "Media feed", icon: "icon_help.svg", hasDot: false },
+        { id: 14, title: "Games", icon: "icon_help.svg", hasDot: false },
+        { id: 15, title: "Dating", icon: "icon_help.svg", hasDot: false }
+    ].map(item => ({ ...item, type: 'icon' }));
 
     let isExpanded = false;
 
     function toggleExpanded() {
         isExpanded = !isExpanded;
     }
-    onMount(() => {
-        const icons = document.querySelectorAll('.social-icon-container');
-        icons.forEach(icon => {
-            icon.addEventListener('touchstart', handleDragStart, false);
-            icon.addEventListener('touchmove', handleDragEnter, false);
-            icon.addEventListener('touchend', handleDragEnd, false);
-        });
-
-        return () => {
-            // Odebrání posluchačů událostí při zničení komponenty
-            icons.forEach(icon => {
-                icon.removeEventListener('touchstart', handleDragStart);
-                icon.removeEventListener('touchmove', handleDragEnter);
-                icon.removeEventListener('touchend', handleDragEnd);
-            });
-        };
-    });
+    import {flip} from "svelte/animate";
+    const flipDurationMs = 300;
+    function handleDndConsider(e) {
+        items = e.detail.items;
+    }
+    function handleDndFinalize(e) {
+        items = e.detail.items;
+    }
 </script>
-
-<div class="social-icons" class:expanded={isExpanded}>
-    <div class="social-icons-block">
-        <!-- Conversations Icon -->
-        <div class="social-icon-container" data-index="0" title="Conversations" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_nemp_v4.svg" alt="Conversations" class="icon" draggable="false"/>
-            <div class="dot">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="12" cy="12" r="6" fill="red"/>
-                </svg>
-            </div>
-        </div>
-
-        <!-- Forum Icon -->
-        <div class="social-icon-container" data-index="1" title="Forum" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Forum" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Events Icon -->
-        <div class="social-icon-container" data-index="2" title="Events" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_event.svg" alt="Events" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Blogs Icon -->
-        <div class="social-icon-container" data-index="3" title="Blogs" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Blogs" class="icon" draggable="false"/>
-        </div>
-
-        <!-- File sharing Icon -->
-        <div class="social-icon-container" data-index="4" title="File sharing" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_file_sharing.svg" alt="File sharing" class="icon" draggable="false"/>
-        </div>
-
-        <!-- E-shops Icon -->
-        <div class="social-icon-container" data-index="5" title="E-shops" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="E-shops" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Marketplace (with auctions) Icon -->
-        <div class="social-icon-container" data-index="6" title="Marketplace (with auctions)" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Marketplace" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Maps Icon -->
-        <div class="social-icon-container" data-index="7" title="Maps" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_map_pin.svg" alt="Maps" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Calendar Icon -->
-        <div class="social-icon-container" data-index="8" title="Calendar" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_calendar.svg" alt="Calendar" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Tasks Icon -->
-        <div class="social-icon-container" data-index="9" title="Tasks" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_task.svg" alt="Tasks" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Donations Icon -->
-        <div class="social-icon-container" data-index="10" title="Donations" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_donations.svg" alt="Donations" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Surveys Icon -->
-        <div class="social-icon-container" data-index="11" title="Surveys" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Surveys" class="icon" draggable="false"/>
-        </div>
-
-
-        <!-- Payments Icon -->
-        <div class="social-icon-container" data-index="12" title="Payments" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Payments" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Media feed Icon -->
-        <div class="social-icon-container" data-index="13" title="Media feed" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Media feed" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Games Icon -->
-        <div class="social-icon-container" data-index="14" title="Games" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Games" class="icon" draggable="false"/>
-        </div>
-
-        <!-- Dating Icon -->
-        <div class="social-icon-container" data-index="15" title="Dating" draggable="true"
-             on:dragstart={handleDragStart}
-             on:dragover={handleDragOver}
-             on:drop={handleDrop}
-             on:dragenter={handleDragEnter}
-             on:dragend={handleDragEnd}>
-            <img src="img/icons/icon_help.svg" alt="Dating" class="icon" draggable="false"/>
-        </div>
-    </div>
-    <div class="social-arrow" class:expanded={isExpanded} on:click={toggleExpanded}></div>
-</div>
 
 <style>
     .dot {
@@ -256,4 +44,23 @@
         height: 24px;
         z-index: 11;
     }
+
 </style>
+
+<div class="social-icons" class:expanded={isExpanded}>
+    <div class="social-icons-block" use:dndzone={{ items }} on:consider="{handleDndConsider}" on:finalize="{handleDndFinalize}">
+        {#each items as item (item.id)}
+            <div class="social-icon-container" animate:flip="{{duration: 200}}" title={item.title}>
+                <img src={`img/icons/${item.icon}`} alt={item.title} class="icon"/>
+                {#if item.hasDot}
+                    <div class="dot">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="6" fill="red"/>
+                        </svg>
+                    </div>
+                {/if}
+            </div>
+        {/each}
+    </div>
+    <div class="social-arrow" class:expanded={isExpanded} on:click={toggleExpanded}></div>
+</div>
