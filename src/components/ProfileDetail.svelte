d5c0a297d7252875e71a724ba6ca57853245fade
bizavladimir <fladirmacht@gmail.com>
2023-08-29 00:30:24 +0200

    ..

--- src/components/ProfileDetail.svelte
+++ src/components/ProfileDetail.svelte
@@ -5,6 +5,7 @@
     } from "../stores/mainstore.js";
 
     export let contactId;
+
     function backButtonClick() {
         contactSelected.set(false);
         activeContactIdStore.set(null);
@@ -15,68 +16,75 @@
 </script>
 <div class="profile-container">
     <div class="back-button">
-        <a class="icon" on:click={backButtonClick}><img src="img/icons/icon_back.svg" alt="search" /></a
+        <a class="icon" on:click={backButtonClick}><img src="img/icons/icon_back.svg" alt="search"/></a
         >
     </div>
 
-<div class="profile-detail">
-    <!-- První div -->
-    <div style="display: flex; width: 100%;">
-        <div class="profile-photo-container">
-            <img class="photo-circle photo-circle-large" src="https://i.pravatar.cc/300?u=ownprofile" alt="Profile Photo">
+    <div class="profile-detail">
+        <!-- První div -->
+        <div class="profile-main">
+            <div class="profile-photo-info">
+                <div class="profile-photo-container">
+                    <img class="photo-circle photo-circle-large" src="https://i.pravatar.cc/300?u=ownprofile"
+                         alt="Profile Photo">
+                </div>
+                <div class="profile-info">
+                    <div class="user-name"><b>Jane Smith</b></div>
+                    <div class="user-email">seznam@radsianineznam.cz</div>
+                </div>
+            </div>
+            <div class="profile-menu">
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Icon 1"></div>
+                    <div><span>Add contact</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Icon 2"></div>
+                    <div><span>Follow</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_message_n_d.svg" alt="Icon 3"></div>
+                    <div><span>Send message</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_donations_n_d.svg" alt="Icon 4"></div>
+                    <div><span>Donate</span></div>
+                </div>
+            </div>
         </div>
-        <div class="profile-menu" >
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Icon 1"></div>
-                <div><span>Add contact</span></div>
+        <!-- Druhý div -->
+        <div class="second-container" style="width: 100%;">
+            <div class="second-container-semi" style="display: flex; justify-content: space-between;">
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_newsfeed_d.svg" alt="Newsfeed Icon"></div>
+                    <div><span>Newsfeed</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_play_out_d.svg" alt="Video Icon"></div>
+                    <div><span>Video</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Forum Icon"></div>
+                    <div><span>Forum</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_event_n_d.svg" alt="Events Icon"></div>
+                    <div><span>Events</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Blog Icon"></div>
+                    <div><span>Blog</span></div>
+                </div>
+                <div class="element">
+                    <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Marketplace Icon"></div>
+                    <div><span>Marketplace</span></div>
+                </div>
             </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Icon 2"></div>
-                <div><span>Follow</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_message_n_d.svg" alt="Icon 3"></div>
-                <div><span>Send message</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_donations_n_d.svg" alt="Icon 4"></div>
-                <div><span>Donate</span></div>
+            <div class="selected-content">
+                TODO details from profile tabs
             </div>
         </div>
     </div>
-    <!-- Druhý div -->
-    <div class="second-container" style="width: 100%;">
-        <div class="second-container-semi" style="display: flex; justify-content: space-between;">
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_newsfeed_d.svg" alt="Newsfeed Icon"></div>
-                <div><span>Newsfeed</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_play_out_d.svg" alt="Video Icon"></div>
-                <div><span>Video</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Forum Icon"></div>
-                <div><span>Forum</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_event_n_d.svg" alt="Events Icon"></div>
-                <div><span>Events</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Blog Icon"></div>
-                <div><span>Blog</span></div>
-            </div>
-            <div class="element">
-                <div class="imgdiv"><img src="./img/icons/icon_help_n_d.svg" alt="Marketplace Icon"></div>
-                <div><span>Marketplace</span></div>
-            </div>
-        </div>
-        <div class="selected-content">
-            TODO details from profile tabs
-        </div>
-    </div>
-</div>
 </div>
 <style>
     .profile-detail {
@@ -85,11 +93,19 @@
         overflow-x: hidden;
         padding-top: 16px;
     }
+    .profile-detail .profile-main {
+        display: flex;
+        width: 100%;
+        overflow: hidden;
+        flex-wrap: wrap;
+    }
+
     .profile-container {
         width: 100%;
         display: flex;
         flex-direction: column;
     }
+
     .profile-detail > div:first-child {
         display: flex;
         width: 100%;
@@ -99,10 +115,12 @@
         padding: 0 32px 32px 32px;
 
     }
+
     .profile-detail > div:last-child > div:first-child {
         display: flex;
         justify-content: space-between;
     }
+
     .second-container-semi {
         display: flex;
         flex-wrap: wrap; /* Povolí zalomení položek flexboxu */
@@ -110,9 +128,10 @@
         container-type: inline-size;
         box-shadow: var(--basic-shadow);
     }
+
     .second-container-semi .element {
         /* min-width: 125px; */
-        width: calc(16.66% - 1px)!important;
+        width: calc(16.66% - 1px) !important;
         flex-grow: 1;
         flex-basis: calc(16.66% - 5px);
         max-width: calc(16.66% - 1px);
@@ -125,68 +144,93 @@
             max-width: calc(33.33% - 1px);
         }
     }
-
+    .profile-photo-info {
+        display: flex;
+        overflow: hidden;
+        flex-wrap: wrap;
+    }
+    .profile-photo-info .profile-info {
+        overflow: hidden;
+    }
+    .profile-photo-info .profile-info .user-name {
+        font-size: 1.2em;
+    }
+    .profile-photo-info .profile-info .user-email {
+        font-size: 1em;
+    }
     .profile-photo-container {
-        padding:0px 0px 0px 0px;
+        padding: 0px 0px 0px 0px;
         width: 200px;
     }
+
     .profile-menu {
         flex-grow: 1;
+        min-width:150px;
         max-width: 200px;
         overflow: hidden;
         box-sizing: border-box;
-        height:200px;
+        height: 200px;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
     }
+
     .profile-menu .element {
         border-radius: 5px;
         margin: 8px;
 
-        max-height:32px;
+        max-height: 32px;
         box-shadow: 0 0 8px 2px #1d1d1d3d;
     }
-    .profile-photo-container img{
-        width:200px;
-        height:200px;
+
+    .profile-photo-container img {
+        width: 200px;
+        height: 200px;
         box-shadow: var(--basic-shadow);
     }
+
     .element {
         width: auto;
-        display: flex;  /* Opravený překlep */
-        align-items: center;  /* Zarovnání položek vertikálně do středu */
-        justify-content: flex-start;  /* Ikonu vlevo a tlačítko vpravo */
+        display: flex; /* Opravený překlep */
+        align-items: center; /* Zarovnání položek vertikálně do středu */
+        justify-content: flex-start; /* Ikonu vlevo a tlačítko vpravo */
         background-color: var(--primary-color-p15);
         border: 1px solid var(--primary-color-15);
-        height:32px;
+        height: 32px;
         margin: 0px 0px 0px 0px;
         flex: 1;
         color: #1d1d1d;
     }
+
     .element div {
     }
+
     .element img {
     }
+
     .imgdiv {
-        width:32px;
-        height:32px;
+        width: 32px;
+        height: 32px;
     }
+
     .selected-content {
         margin-top: 25px;
         text-align: center;
     }
+
     .back-button {
         height: var(--userbar-height);
         display: flex;
         align-items: center;
         padding-left: 10px;
     }
+
     .back-button a {
         height: var(--userbar-height);
     }
+
     .back-button img {
-        width:48px;
-        height:48px;
+        width: 48px;
+        height: 48px;
     }
 </style>
