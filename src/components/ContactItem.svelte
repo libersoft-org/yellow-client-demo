151c930d4bb40354ee43ac273f4e6e6ae5776ea8
bizavladimir <fladirmacht@gmail.com>
2023-08-29 01:40:50 +0200

    ..

--- src/components/ContactItem.svelte
+++ src/components/ContactItem.svelte
@@ -29,8 +29,46 @@
             />
         </div>
     {/if}
-    <div class="conversation__info">
+    <div class="contact-info">
         <div class="conversation__info__user-name">{contact.name}</div>
         <div class="conversation__info__user-email">{contact.email}</div>
     </div>
 </div>
+<style>
+    .contact-info {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-orient: vertical;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        justify-content: flex-start;
+        padding: 0px 10px;
+        -webkit-box-flex: 3;
+        -ms-flex-positive: 3;
+        flex-grow: 3;
+        -ms-flex-negative: 1;
+        flex-shrink: 1;
+        -ms-flex-preferred-size: 0%;
+        flex-basis: 0%;
+        overflow: hidden;
+    }
+    .conversation__info__user-name {
+        font-weight: bold;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+
+    .conversation__info__user-email {
+        font-size: 0.8rem;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        margin-bottom: 8px;
+        font-weight: normal ;
+    }
+</style>
