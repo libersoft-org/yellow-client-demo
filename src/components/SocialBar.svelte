a4c1ee758122bf5762b232f647ecd7cf748f596c
Fladir <fladirm@gmail.com>
2023-09-26 17:29:34 +0200

    ...

--- src/components/SocialBar.svelte
+++ src/components/SocialBar.svelte
@@ -4,17 +4,16 @@
     import VideoMeeting from "./VideoMeeting.svelte";
 
     let showVideoMeet = false;
-    let isloaded = false;
     let items = [
         {
-            id: 0, title: "Conversations", icon: "icon_nemp_v4.svg", hasDot: true, onclick: (e) => {
+            id: 0, title: "Conversations", icon: "icon_nemp_v4_filled.svg", hasDot: true, onclick: (e) => {
                 e.preventDefault();
                 $actualMVC = 'conversation';
                 document.querySelector('#menu-toggle').click()
             }
         },
         {
-            id: 1, title: "Newsfeed", icon: "icon_newsfeed.svg", hasDot: true, onclick: (e) => {
+            id: 1, title: "Newsfeed", icon: "icon_newsfeed_filled.svg", hasDot: true, onclick: (e) => {
                 e.preventDefault();
                 $actualMVC = 'newsfeed';
                 document.querySelector('#menu-toggle').click();
@@ -24,7 +23,7 @@
             }
         },
         {
-            id: 2, title: "Video", icon: "icon_play_out.svg", hasDot: true, onclick: (e) => {
+            id: 2, title: "Video", icon: "icon_play_filled.svg", hasDot: true, onclick: (e) => {
                 e.preventDefault();
                 $actualMVC = 'video';
                 $activeVideView = 'videolist';
@@ -35,26 +34,26 @@
             }
         },
         {
-            id: 3, title: "Video meeting", icon: "icon_video_meeting.svg", hasDot: true, onclick: () => {
+            id: 3, title: "Video meeting", icon: "icon_video_meeting_filled.svg", hasDot: true, onclick: () => {
                 showVideoMeet = true;
             }
         },
-        {id: 4, title: "Forum", icon: "icon_help_n.svg", hasDot: false},
-        {id: 5, title: "Events", icon: "icon_event_n.svg", hasDot: false},
-        {id: 6, title: "Blogs", icon: "icon_help_n.svg", hasDot: false},
-        {id: 7, title: "File sharing", icon: "icon_file_sharing_n.svg", hasDot: false},
-        {id: 8, title: "E-shops", icon: "icon_help_n.svg", hasDot: false},
-        {id: 9, title: "Marketplace (with auctions)", icon: "icon_help_n.svg", hasDot: false},
-        {id: 10, title: "Maps", icon: "icon_map_pin_n.svg", hasDot: false},
-        {id: 11, title: "Calendar", icon: "icon_calendar_n.svg", hasDot: false},
-        {id: 12, title: "Tasks", icon: "icon_task_n.svg", hasDot: false},
-        {id: 13, title: "Donations", icon: "icon_donations_n.svg", hasDot: false},
-        {id: 14, title: "Crowdfunding", icon: "icon_help_n.svg", hasDot: false},
-        {id: 15, title: "Cloud storage", icon: "icon_file_n.svg", hasDot: true},
-        {id: 16, title: "Payments", icon: "icon_help_n.svg", hasDot: false},
-        {id: 17, title: "Media feed", icon: "icon_help_n.svg", hasDot: false},
-        {id: 18, title: "Games", icon: "icon_help_n.svg", hasDot: false},
-        {id: 19, title: "Dating", icon: "icon_help_n.svg", hasDot: false}
+        {id: 4, title: "Forum", icon: "icon_forum_filled.svg", hasDot: false},
+        {id: 5, title: "Events", icon: "icon_event_filled.svg", hasDot: false},
+        {id: 6, title: "Blogs", icon: "icon_blog_filled.svg", hasDot: false},
+        {id: 7, title: "File sharing", icon: "icon_file_sharing_filled.svg", hasDot: false},
+        {id: 8, title: "E-shops", icon: "icon_e-shop_filled.svg", hasDot: false},
+        {id: 9, title: "Marketplace (with auctions)", icon: "icon_marketplace_filled.svg", hasDot: false},
+        {id: 10, title: "Maps", icon: "icon_map_pin_filled.svg", hasDot: false},
+        {id: 11, title: "Calendar", icon: "icon_calendar_filled.svg", hasDot: false},
+        {id: 12, title: "Tasks", icon: "icon_task.svg", hasDot: false},
+        {id: 13, title: "Donations", icon: "icon_donations.svg", hasDot: false},
+        {id: 14, title: "Crowdfunding", icon: "icon_crowdfunding.svg", hasDot: false},
+        {id: 15, title: "Cloud storage", icon: "icon_cloud_storage.svg", hasDot: true},
+        {id: 16, title: "Payments", icon: "icon_payments.svg", hasDot: false},
+        {id: 17, title: "Media feed", icon: "icon_mediafeed.svg", hasDot: false},
+        {id: 18, title: "Games", icon: "icon_games.svg", hasDot: false},
+        {id: 19, title: "Dating", icon: "icon_dating.svg", hasDot: false}
     ];
 
     let isExpanded = false;
@@ -62,10 +61,12 @@
     function toggleExpanded() {
         isExpanded = !isExpanded;
     }
+
     function handleDndConsider(e) {
         e.preventDefault();
         items = e.detail.items;
     }
+
     function handleDndFinalize(e) {
         e.preventDefault();
         items = e.detail.items;
@@ -86,22 +87,24 @@
 
 <div class="social-icons" class:expanded={isExpanded}>
     {#if dndzone }
-    <div class="social-icons-block" use:dndzone={{ items }} on:click={()=>{console.log('www');}} on:consider={handleDndConsider}
-         on:finalize={handleDndFinalize}>
-        {#each items as item (item.id)}
-            <div class="social-icon-container" title={item.title} on:click={item.onclick}>
-                <img src={`img/icons/${item.icon}`} alt={item.title} class="icon"/>
-                {#if item.hasDot}
-                    <div class="dot">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="12" cy="12" r="6" fill="red"/>
-                        </svg>
-                    </div>
-                {/if}
-            </div>
-        {/each}
-    </div>
-        {/if}
+        <div class="social-icons-block" use:dndzone={{ items }} on:click={()=>{console.log('www');}}
+             on:consider={handleDndConsider}
+             on:finalize={handleDndFinalize}>
+            {#each items as item (item.id)}
+                <div class="social-icon-container" title={item.title} on:click={item.onclick}>
+                    <img src={`img/icons_new/${item.icon}`} alt={item.title} class="icon"/>
+                    {#if item.hasDot}
+                        <div class="dot">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="12" cy="12" r="6" fill="red"/>
+                            </svg>
+                        </div>
+                    {/if}
+                </div>
+            {/each}
+        </div>
+    {/if}
     <div class="social-arrow" class:expanded={isExpanded} on:click={toggleExpanded}></div>
 </div>
 {#if showVideoMeet} }
