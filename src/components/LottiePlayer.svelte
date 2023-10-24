1c7f50e9b713fa5410d4804f0f4cb8546c60cd7a
vladibiz <bizavladimir@gmail.com>
2023-10-24 15:48:39 +0200

    static

--- src/components/LottiePlayer.svelte
+++ src/components/LottiePlayer.svelte
@@ -1,25 +1,30 @@
 <script>
-	import { onMount } from 'svelte';
-	import lottie from 'lottie-web';
+    import { onMount } from 'svelte';
 
-	export let animationData = null;
-	let container;
+    let lottie;
+    export let animationData = null;
+    let container;
 
-	onMount(() => {
-		if (animationData && container) {
-			lottie.loadAnimation({
-				container: container, // odkaz na DOM element
-				renderer: 'svg', // 'svg', 'canvas', 'html'
-				loop: true, // smyčka animace
-				autoplay: true, // automatické přehrávání
-				animationData: animationData // předaná animační data
-			});
-		}
-	});
+    onMount(() => {
+        if (typeof window !== 'undefined') {
+            import('lottie-web').then(module => {
+                lottie = module.default;
+                if (animationData && container) {
+                    lottie.loadAnimation({
+                        container: container, // odkaz na DOM element
+                        renderer: 'svg', // 'svg', 'canvas', 'html'
+                        loop: true, // smyčka animace
+                        autoplay: true, // automatické přehrávání
+                        animationData: animationData // předaná animační data
+                    });
+                }
+            });
+        }
+    });
 </script>
 
 <div class="lottie" bind:this={container} />
 
 <style>
-	/* Zde můžete přidat vlastní styly pro kontejner, pokud je to potřeba */
+    /* Zde můžete přidat vlastní styly pro kontejner, pokud je to potřeba */
 </style>
